834ccea525a0f7b34be38ae91365966b1a6db0ca
MK Safi <msafi@msafi.com>
2014-07-09 17:01:55 +0300

    Now we can read an image file on the front end. Next up: upload to Amazon S3.

--- app.js
+++ app.js
@@ -1,37 +1,27 @@
-//inject angular file upload directives and service.
-angular.module('myApp', ['angularFileUpload']);
+angular.module('myApp', ['angularFileUpload'])
 
-var MyCtrl = [ '$scope', '$upload', function($scope, $upload) {
-  $scope.onFileSelect = function($files) {
-    //$files: an array of files selected, each file has name, size, and type.
-    for (var i = 0; i < $files.length; i++) {
-      var file = $files[i];
-      $scope.upload = $upload.upload({
-        url: 'server/upload/url', //upload.php script, node.js route, or servlet url
-        // method: 'POST' or 'PUT',
-        // headers: {'header-key': 'header-value'},
-        // withCredentials: true,
-        data: {myObj: $scope.myModelObj},
-        file: file, // or list of files: $files for html5 only
-        // fileName: 'doc.jpg' or ['1.jpg', '2.jpg', ...] // to modify the name of the file
-        /* customize file formData name ('Content-Desposition'), server side file variable name.
-         Default is 'file' */
-        //fileFormDataName: myFile, //or a list of names for multiple files (html5).
-        /* customize how data is added to formData. See #40#issuecomment-28612000 for sample code */
-        //formDataAppender: function(formData, key, val){}
-      }).progress(function(evt) {
-        console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
-      }).success(function(data, status, headers, config) {
-        // file is uploaded successfully
-        console.log(data);
-      });
-      //.error(...)
-      //.then(success, error, progress);
-      //.xhr(function(xhr){xhr.upload.addEventListener(...)})// access and attach any event listener to XMLHttpRequest.
+.controller('MyCtrl',
+  function($scope, $upload) {
+    $scope.onFileSelect = function(files) {
+      var fileReader = new FileReader
+
+      fileReader.onload = function() {
+        var img = new Image
+
+        img.onload = function() {
+          $scope.$apply(function() {
+            $scope.uploadedImg = {
+              width: img.width + 'px',
+              height: img.height + 'px',
+              src: img.src
+            }
+          })
+        }
+
+        img.src = fileReader.result
+      }
+
+      fileReader.readAsDataURL(files[0])
     }
-    /* alternative way of uploading, send the file binary with the file's content-type.
-     Could be used to upload files to CouchDB, imgur, etc... html5 FileReader is needed.
-     It could also be used to monitor the progress of a normal http post/put request with large data*/
-    // $scope.upload = $upload.http({...})  see 88#issuecomment-31366487 for sample code.
-  };
-}];
+  }
+)
